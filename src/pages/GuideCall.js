88bc911de53a90a230e6295163ecfaac0055dd0e
elysdominguez <elysdominguez2@gmail.com>
2022-10-10 13:55:18 +0200

    conect the title call with useSelector by city id and I did some Css

--- src/pages/GuideCall.js
+++ src/pages/GuideCall.js
@@ -1,18 +1,25 @@
 import "./style.css";
 import { ImPhoneHangUp } from "react-icons/im";
 import { ImPhone } from "react-icons/im";
+import { selectCityById } from "../store/cities/selectors";
+import { useSelector } from "react-redux";
 
 export const GuideCall = () => {
+  const city = useSelector(selectCityById);
   return (
     <div className="container">
       <div className="container-guide-call">
-        <h3 className="title-call">Amsterdam Guide</h3>
+        <h3 className="title-call">{city.name} Guide</h3>
         <div className="buttons-call">
-          <button className="button-call">
-            <ImPhoneHangUp />
+          <button className="button-call red">
+            <p className="icone-phone">
+              <ImPhoneHangUp />
+            </p>
           </button>
-          <button className="button-call">
-            <ImPhone />
+          <button className="button-call green">
+            <p className="icone-phone">
+              <ImPhone />
+            </p>
           </button>
         </div>
       </div>
