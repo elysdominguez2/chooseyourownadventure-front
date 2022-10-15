b635462ab670399d98f2fb288454622c23ebcf41
elysdominguez <elysdominguez2@gmail.com>
2022-10-15 12:00:08 +0200

    Navigation styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cust-purple": "#d8b5ce",
+        "cust-yellow": "#f4d92c",
+      },
+    },
   },
   plugins: [],
 };
