3560bf99a40707e3eb14705cc81e4b12bbe6f361
elysdominguez <elysdominguez2@gmail.com>
2022-10-16 17:16:29 +0200

    Sign up Page styling with Tailwind

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,21 +1,18 @@
-import styled from "styled-components"
-import { Button, Input, Title } from "../styled"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { signUp } from "../store/user/thunks"
-import { selectToken } from "../store/user/selectors"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signUp } from "../store/user/thunks";
+import { selectToken } from "../store/user/selectors";
 
 export const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  const token = useSelector(selectToken)
+  const token = useSelector(selectToken);
 
   useEffect(() => {
     if (token !== null) {
@@ -24,41 +21,54 @@ export const SignUp = () => {
   }, [token, navigate]);
 
   const submitForm = (e) => {
-    e.preventDefault()
-    dispatch(signUp(name, email, password))
-  }
+    e.preventDefault();
+    dispatch(signUp(name, email, password));
+  };
 
   return (
-    <div style={{textAlign: "center"}}>
-      <Container>
-        <Title>Sign Up</Title>
-        <form onSubmit={submitForm}>
-          <Input 
+    <div>
+      <div className=" flex flex-col justify-center items-center">
+        <h3 className="text-2xl py-8 text-center font-Lato font-semibold text-cust-dark-purple">
+          Sign Up
+        </h3>
+        <form
+          onSubmit={submitForm}
+          className=" flex flex-col justify-center items-center"
+        >
+          <input
             placeholder="name"
-            value={name} 
+            value={name}
             onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
           />
-          <Input 
+          <input
             placeholder="email"
-            value={email} 
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
           />
-          <Input 
-            type="password" 
+          <input
+            type="password"
             placeholder="password"
-            value={password} 
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
           />
-          <br/>
-          <Button type="submit">Sign Up</Button>
+          <br />
+          <button
+            type="submit"
+            className=" bg-yellow-500 hover:bg-yellow-400 border-b-4 border-yellow-700 hover:border-yellow-500 text-white text-center text-xl my-4 py-2 px-4 rounded"
+          >
+            Sign Up
+          </button>
         </form>
-      </Container>
+      </div>
     </div>
-  )
-}
-
-const Container = styled.div`
-  display: 'flex';
-  flex-direction: 'column';
-  margin: 15%;
-`
+  );
+};
