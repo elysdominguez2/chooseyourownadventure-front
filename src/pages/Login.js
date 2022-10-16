8b17d749a19da49eadea28178d07687bf58bb9a3
elysdominguez <elysdominguez2@gmail.com>
2022-10-16 17:08:06 +0200

    Login page styling with Tailwind

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,18 @@
-import styled from "styled-components"
-import { Button, Input, Title, LinkWord } from "../styled"
-import { Link } from "react-router-dom"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { login } from "../store/user/thunks"
-import { selectToken } from "../store/user/selectors"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../store/user/thunks";
+import { selectToken } from "../store/user/selectors";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  const token = useSelector(selectToken)
+  const token = useSelector(selectToken);
 
   useEffect(() => {
     if (token !== null) {
@@ -24,46 +21,53 @@ export const Login = () => {
   }, [token, navigate]);
 
   const submitForm = (e) => {
-    e.preventDefault()
-    dispatch(login(email, password))
-  }
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
 
   return (
-    <div style={{textAlign: "center"}}>
-      <Container>
-        <Title>Login</Title>
-        <form onSubmit={submitForm}>
-          <Input 
+    <div className=" flex flex-col justify-center items-center">
+      <div>
+        <h3 className="text-2xl py-8 text-center font-Lato font-semibold text-cust-dark-purple">
+          Login
+        </h3>
+        <form
+          onSubmit={submitForm}
+          className=" flex flex-col justify-center items-center"
+        >
+          <input
             placeholder="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
           />
-          <Input 
-            type="password" 
+          <input
+            type="password"
             placeholder="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
           />
-          <br/>
-          <Button type="submit">Login</Button>
+          <br />
+          <button
+            type="submit"
+            className=" bg-yellow-500 hover:bg-yellow-400 border-b-4 border-yellow-700 hover:border-yellow-500 text-white text-center text-xl my-4 py-2 px-4 rounded"
+          >
+            Login
+          </button>
         </form>
-        <SubText>
-          Don't have an account yet? Click <Link to="/signup" style={LinkWord}>here</Link> to sign up
-        </SubText>
-      </Container>
+        <div className="text-center text-cust-dark-purple pt-4 pb-2">
+          Don't have an account yet? Click{" "}
+          <Link to="/signup" className="text-cust-orange">
+            here
+          </Link>{" "}
+          to sign up
+        </div>
+      </div>
     </div>
-  )
-}
-
-const Container = styled.div`
-  display: 'flex';
-  flex-direction: 'column';
-  margin: 15%;
-`
-
-const SubText = styled.p`
-  text-align: center;
-  color: #1E3163;
-  padding: 20px 0px 5px 0px;
-`;
-
+  );
+};
