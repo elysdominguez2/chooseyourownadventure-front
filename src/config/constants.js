0f17c46cb5e1f9aa3047f490f85f34d53a16176e
Eliana Dominguez <68299455+elysdominguez2@users.noreply.github.com>
2023-01-17 18:32:48 +0100

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = process.env.API_URL || "https://choose-own-adventure.onrender.com/";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
